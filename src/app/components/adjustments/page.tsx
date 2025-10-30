1bfbe5728a1be121e9447b6f0b9469cdd58fad5e
muhammadzaheer123 <zaheerkhan62145@gmail.com>
2025-10-31 03:47:35 +0500

    First

--- src/app/components/adjustments/page.tsx
+++ src/app/components/adjustments/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <>
       <div className="bg-[#1C1039] w-full h-[100vh] mx-auto flex justify-center align-middle">
-        <div className="w-[80%] h-[100%] m-auto bg-amber-100"> dhuadh</div>
+        <div className="w-[80%] h-[100%] m-auto bg-amber-100">skhajs</div>
       </div>
     </>
   );
